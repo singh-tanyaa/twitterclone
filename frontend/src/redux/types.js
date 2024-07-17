03e169eab755752133d2249715eef35f4d416ace
singh-tanyaa <96328928+singh-tanyaa@users.noreply.github.com>
2024-07-17 15:57:35 +0530

    frontend again

--- frontend/src/redux/types.js
+++ frontend/src/redux/types.js
@@ -1,2 +1,2 @@
-export const BOOKMARK_TOGGLE = 'BOOKMARK_TOGGLE';
-export const GET_BOOKMARKS = 'GET_BOOKMARKS';
+// export const BOOKMARK_TOGGLE = 'BOOKMARK_TOGGLE';
+// export const GET_BOOKMARKS = 'GET_BOOKMARKS';
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
 //api ENDPOINTS
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitterclone-uypv.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitterclone-uypv.onrender.com/api/v1/tweet";
 
 export const timeSince = (timestamp) => {
     let time = Date.parse(timestamp);
